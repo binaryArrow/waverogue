ec2c46c32315f726c0f036f060bbd5aba8ad47d3
binaryArrow <cancihan96@gmail.com>
2022-05-20 20:17:08 +0200

    add: based on time animation TODO: - subtract fps at gameloop, to handle different framerates - handle velocity

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import {Player} from "./models/Player";
 
-let secondsPassed
-let oldTimeStamp: number
+let secondsPassed: number = 0
+let oldTimeStamp: number = 0
 let fps
 let canvas: HTMLCanvasElement
 let context: CanvasRenderingContext2D
-let x = 0
-let y = 0
 let player: Player
 window.onload = init
 
@@ -15,29 +13,26 @@ window.onload = init
 function init() {
     canvas = document.getElementById('gridCanvas') as HTMLCanvasElement
     context = canvas.getContext('2d') as CanvasRenderingContext2D
-    player = new Player(context, x, y)
-    window.requestAnimationFrame(update)
-    draw()
+    player = new Player(context, 0, 0, 2)
+    window.requestAnimationFrame(loop)
 }
 
-function update(timeStamp: number){
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    draw()
-    drawFps(timeStamp)
-    window.requestAnimationFrame(update)
-}
-
-function draw() {
-    player.draw()
-    player.posX = x
-    player.posY = y
-    x ++
-    y ++
-}
-
-function drawFps(timeStamp: number){
+function loop(timeStamp: number){
     secondsPassed = (timeStamp - oldTimeStamp) / 1000
     oldTimeStamp = timeStamp
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    update(secondsPassed)
+    drawFps()
+    window.requestAnimationFrame(loop)
+}
+
+function update(passedSeconds: number){
+    player.posX += (player.movementSpeed * passedSeconds)
+    player.posY += (player.movementSpeed * passedSeconds)
+}
+
+function drawFps(){
     fps = Math.round(1 / secondsPassed)
     context.font = '15px Arial';
     context.fillStyle = 'black';
