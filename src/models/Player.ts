781a45c9cd3a7a094a78c4a4b3aa914cb306efdc
binaryArrow <cancihan96@gmail.com>
2022-05-20 20:17:08 +0200

    add: based on time animation TODO: - subtract fps at gameloop, to handle different framerates - handle velocity

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,5 +1,10 @@
 import {Sprite} from "./Sprite";
 
 export class Player extends Sprite {
+    movementSpeed
+    constructor(context: CanvasRenderingContext2D, posX: number, posY: number, movementSpeed: number) {
+        super(context, posX, posY);
+        this.movementSpeed = movementSpeed
+    }
 
 }
