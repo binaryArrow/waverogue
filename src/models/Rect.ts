df22843995d28421888fe825c966cb12fd8c1b94
binaryArrow <cancihan96@gmail.com>
2022-06-06 22:45:59 +0200

    REF: added invisible mode for rects TODO: - add Cooldown on roll - add attack - UI - enemies - add sprites and animation

--- src/models/Rect.ts
+++ src/models/Rect.ts
@@ -16,6 +16,9 @@ export class Rect {
     draw(){
         this.context.fillStyle = this.collides ? '#a66c6c':'#94c781'
         this.context.strokeStyle = '#000000'
+        // this is with no colors
+        // this.context.fillStyle = this.collides ? 'rgba(255,255,255,0)':'rgba(121,211,75,0)'
+        // this.context.strokeStyle = 'rgba(0,0,0,0)'
         this.context.lineWidth = 2
         this.context.fillRect(this.x, this.y, this.width, this.height)
         this.context.strokeRect(this.x, this.y, this.width, this.height)
