d671152c866dbd442c24f9fff5ac2cfe0f2b155f
binaryArrow <cancihan96@gmail.com>
2022-06-16 19:24:20 +0200

    add: crouch

--- src/models/Constants.ts
+++ src/models/Constants.ts
@@ -1,4 +1,6 @@
 export class Constants{
     static playerRollCooldown: number = 1 // in seconds
     static playerAttackSpeed: number = 0.3
+    static playerHeight: number = 50
+    static playerHeightCrouch: number = 25
 }
