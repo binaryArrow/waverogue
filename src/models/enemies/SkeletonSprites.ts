f9152a2beb0254d10789c6eebba98bea500dc584
binaryArrow <cancihan96@gmail.com>
2022-08-13 02:07:27 +0200

    add: sprites for movement todo: - skeleton attack player (simple KI) - add second attack for player - fix dashing to wall: still glitches out of wall when wall is in dashrange

--- src/models/enemies/SkeletonSprites.ts
+++ src/models/enemies/SkeletonSprites.ts
@@ -1,15 +1,27 @@
 import {SpriteSheet} from "../SpriteSheet";
 import skeletonIdleRight from "../../assets/skeleton/skeleton_idle_right.png"
+import skeletonIdleLeft from "../../assets/skeleton/skeleton_idle_left.png"
 import skeletonTakeHitRight from "../../assets/skeleton/take_hit_right.png"
+import skeletonTakeHitLeft from "../../assets/skeleton/take_hit_left.png"
+import skeletonWalkRight from "../../assets/skeleton/walk_right.png"
+import skeletonWalkLeft from "../../assets/skeleton/walk_left.png"
 
 export class SkeletonSprites{
     context: CanvasRenderingContext2D
     spriteSheetIdleRight: SpriteSheet
+    spriteSheetIdleLeft: SpriteSheet
     spriteSheetTakeHitRight: SpriteSheet
+    spriteSheetTakeHitLeft: SpriteSheet
+    spriteSheetWalkLeft: SpriteSheet
+    spriteSheetWalkRight: SpriteSheet
 
     constructor(context: CanvasRenderingContext2D) {
         this.context = context
         this.spriteSheetIdleRight = new SpriteSheet(150, 54, 4, skeletonIdleRight, this.context)
+        this.spriteSheetIdleLeft = new SpriteSheet(150, 54, 4, skeletonIdleLeft, this.context)
         this.spriteSheetTakeHitRight = new SpriteSheet(150, 150, 4, skeletonTakeHitRight, this.context)
+        this.spriteSheetTakeHitLeft = new SpriteSheet(150, 150, 4, skeletonTakeHitLeft, this.context)
+        this.spriteSheetWalkRight = new SpriteSheet(150, 150, 4, skeletonWalkRight, this.context)
+        this.spriteSheetWalkLeft = new SpriteSheet(150, 150, 4, skeletonWalkLeft, this.context)
     }
 }
