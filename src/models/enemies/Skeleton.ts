5df66d4845a34957896a2d6943d706802164f596
binaryArrow <cancihan96@gmail.com>
2022-08-13 02:07:27 +0200

    add: sprites for movement todo: - skeleton attack player (simple KI) - add second attack for player - fix dashing to wall: still glitches out of wall when wall is in dashrange

--- src/models/enemies/Skeleton.ts
+++ src/models/enemies/Skeleton.ts
@@ -53,23 +53,42 @@ export class Skeleton extends GameObject implements Character {
             this.context.strokeRect(this.posX, this.posY, this.width, this.height)
     }
 
+
+    updateMovement(secondsPassed: number): void {
+        if (this.otherGemObject.posX + this.otherGemObject.width < this.posX) {
+            this.moveLeft()
+            this.faceDirection = FaceDirection.LEFT
+        } else if (this.otherGemObject.posX > this.posX + this.width) {
+            this.moveRight()
+            this.faceDirection = FaceDirection.RIGHT
+        }
+        else this.stopMovingXAxis()
+        console.log(this.velocityX)
+        console.log(this.movementSpeed)
+    }
+
     animate(): void {
-        if (!this.hit)
+        if (!this.hit && this.faceDirection == FaceDirection.RIGHT && this.velocityX == 0)
             this.sprites.spriteSheetIdleRight.animate(12, this.posX - 65, this.posY + 34, 180, 100, this.width, this.height)
 
-        else if (this.hit) {
+        else if (!this.hit && this.faceDirection == FaceDirection.LEFT && this.velocityX == 0)
+            this.sprites.spriteSheetIdleLeft.animate(12, this.posX - 45, this.posY + 34, 180, 100, this.width, this.height)
+
+        else if (this.hit && this.faceDirection == FaceDirection.RIGHT) {
             this.stopMovingXAxis()
             this.sprites.spriteSheetTakeHitRight.animate(7, this.posX - 65, this.posY - 48, 180, 270, this.width, this.height)
             if (this.sprites.spriteSheetTakeHitRight.animationFinished()) this.hit = false
         }
-        console.log(this.sprites.spriteSheetTakeHitRight.actualSprite)
-    }
+        else if (this.hit && this.faceDirection == FaceDirection.LEFT) {
+            this.stopMovingXAxis()
+            this.sprites.spriteSheetTakeHitLeft.animate(7, this.posX - 65, this.posY - 48, 180, 270, this.width, this.height)
+            if (this.sprites.spriteSheetTakeHitLeft.animationFinished()) this.hit = false
+        }
+        else if (this.velocityX < 0 && this.faceDirection == FaceDirection.LEFT)
+            this.sprites.spriteSheetWalkLeft.animate(12, this.posX - 65, this.posY - 48, 180, 270, this.width, this.height)
 
-    updateMovement(secondsPast: number): void {
-        if (this.otherGemObject.posX < this.posX)
-            this.moveLeft()
-        else
-            this.moveRight()
+        else if (this.velocityX > 0 && this.faceDirection == FaceDirection.RIGHT)
+            this.sprites.spriteSheetWalkRight.animate(12, this.posX - 65, this.posY - 48, 180, 270, this.width, this.height)
     }
 
     setRollPosition(secondsPast: number): void {
